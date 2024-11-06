e451a4490960cc26b429c5c2f2bd6671d8d50f52
supto <salatussadik131@gmail.com>
2024-11-07 02:12:47 +0530

    complite desktop

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Hotel management
+
+A brief description of what this project does and who it's for
+
+
+## PDF of website
+
+Desktop page1
+https://drive.google.com/file/d/13Ca7Xy1DKkp32XNJsdPi1IIx3OnNSdzE/view?usp=sharing
+
+Mobile page1
+
+## Features
+
+* Responsive design with Tailwind CSS
+
+* remix icons for visual enhancements
+
+* Clean, minimalistic layout and structure
+
+## Technologies Used
+
+* Tailwind CSS: For utility-first styling and responsiveness
+* HTML: The foundational markup of the application
+* remix: Used for icons
+    
--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ const navbarManu = document.querySelector("#nav_manus");
 navbar.addEventListener("click",()=>{
     navbarManu.classList.toggle("navbarOpen")
 })
+
+// swiper
+const swiper = new Swiper(".swiper",{
+    loop:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true
+    },
+    // brackpoints
+    slidesPerView:3,
+    spaceBetween:20,
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+        },
+        960:{
+            slidesPerView:2,
+        },
+        1200:{
+            slidesPerView:3,
+        }
+    }
+})
